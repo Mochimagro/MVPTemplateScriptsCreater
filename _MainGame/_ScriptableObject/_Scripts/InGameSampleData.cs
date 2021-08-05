517784d5b2a601cb2e89aad79abaa364a11a55c3
TT_Numazu <numazu@tokyo-tsushin.com>
2021-08-05 22:01:17 +0900

    Data修正

---
+++ _MainGame/_ScriptableObject/_Scripts/InGameSampleData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace MVPTemplateEditor.Data
+{
+    [CreateAssetMenu(fileName = "InGameSampleData", menuName = "Installers/InGameSampleData")]
+    public class InGameSampleData :  ScriptableObjectInstaller<InGameSampleData>
+    {
+        [SerializeField] string _value = default;
+
+        public string Value { get => _value; }
+
+        public override void InstallBindings()
+        {
+            Container.Bind<InGameSampleData>().FromInstance(this).AsCached();
+        }
+    }
+}
